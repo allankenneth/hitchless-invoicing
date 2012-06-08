d614691fde7ac6d17eccc4c00da2570899abfeb4
allankh <allankh@gmail.com>
2012-06-07 20:06:23 -0700

    catch up adding some random files

---
+++ views/assets/js/hitchless.js
@@ -0,0 +1,20 @@
+!function ($) {
+
+	var panel = window.location.hash;
+	if (!panel) panel = '#projects';
+	var show = '#tab a[href="'+panel+'"]';
+	$(show).tab('show');
+
+	$(".invdetail").popover();
+	$('#showcinfo').modal({backdrop:'false', show:'false'})
+	$('#servicemenu').modal({backdrop:'false', show:'false'});
+    $(".invoiceproject").on('click', function(){
+    	$("#newinvoice").show();
+    });
+	$('.sheet').on('click',function(e){
+		e.preventDefault();
+		foo = $(this).attr("href");
+		$(this).next(".sheetplaceholder").load(foo); 
+	});
+
+}(window.jQuery);
