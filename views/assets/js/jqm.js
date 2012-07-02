3a65ca7478bd9ceded1cbbb4caa181b994884690
allankh <allankh@gmail.com>
2012-07-02 12:25:30 -0700

    Mostly just UI tweaks.

--- views/assets/js/jqm.js
+++ views/assets/js/jqm.js
@@ -6,13 +6,35 @@ $(document).ready(function(){
 	var show = '#tab a[href="'+panel+'"]';
 	$(show).tab('show');
 */
+/*
 	$('.shown').click(function(){
-/* 		e.preventDefault(); */
+		e.preventDefault();
 		foo = $(this).attr("href");
 
 		$(foo).toggle();
 
 	});
+*/
+
+
+
+
+	// attach the plugin to an element
+	$('#timelist li').swipeDelete({
+		btnTheme: 'b',
+		btnLabel: 'Delete',
+		btnClass: 'aSwipeButton',
+		click: function(e){
+			e.preventDefault();
+			var url = $(e.target).attr('href');
+			$(this).parents('li').slideUp();
+			$.post(url, function(data) {
+				console.log(data);
+
+			});
+		}
+	});
+
 
     
 
